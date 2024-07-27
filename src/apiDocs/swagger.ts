5373fcd2d0dbbcc4be4630406a5d53cd8f160a80
starvader13 <ashutoshgupta1311@gmail.com>
2024-07-27 11:14:21 +0530

    Setup swagger configuration and integrate swagger into express through api-docs route
    Signed-off-by: starvader13 <ashutoshgupta1311@gmail.com>

---
+++ src/apiDocs/swagger.ts
@@ -0,0 +1,22 @@
+import swaggerUI from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc'
+import { Application } from 'express';
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Historical Exchange Data REST API",
+            version: "1.0.0",
+            description: "API documentation for the Historical Exchange Data service"
+        },
+    },
+    apis:["../routes/*.ts"]
+}
+const specs = swaggerJsDoc(options);
+
+const setupSwagger = (app: Application) => {
+    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+};
+
+export default setupSwagger
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import mainRouter from './routes/mainRouter';
+import setupSwagger from './apiDocs/swagger';
 dotenv.config();
 
 const app = express();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.use("/api", mainRouter);
+setupSwagger(app);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running at Port ${PORT}`);
