2b7477add518bcae0bae3e7563db9d1bdf558847
starvader13 <ashutoshgupta1311@gmail.com>
2024-07-27 17:48:46 +0530

    Fixes execution of readLogs due to export
    Signed-off-by: starvader13 <ashutoshgupta1311@gmail.com>

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,9 @@
 import historicalExchangeScrapper from "./historicalExchangeScrapper";
 import getTimeFrame from "./getTimeFrame";
 import logger from "./logger";
-import readLogs from "./readLogs";
 
 export {
     historicalExchangeScrapper,
     getTimeFrame,
     logger,
-    readLogs
 };
