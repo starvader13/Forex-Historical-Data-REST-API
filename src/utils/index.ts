869b17c5eae702c3d6d28325bda1f227075eecec
starvader13 <ashutoshgupta1311@gmail.com>
2024-07-27 17:27:10 +0530

    Add function to readLogs from command line
    Signed-off-by: starvader13 <ashutoshgupta1311@gmail.com>

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,11 @@
 import historicalExchangeScrapper from "./historicalExchangeScrapper";
 import getTimeFrame from "./getTimeFrame";
 import logger from "./logger";
+import readLogs from "./readLogs";
 
 export {
     historicalExchangeScrapper,
     getTimeFrame,
-    logger
+    logger,
+    readLogs
 };
---
+++ src/utils/readLogs.ts
@@ -0,0 +1,44 @@
+import path from "path";
+import fs from 'fs';
+import { LogData } from "../types";
+import readline from 'readline';
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+class LogDataRenderer {
+    status: boolean;
+    origin: string;
+    logMessage: string;
+    timestamp: string
+
+    constructor(status: boolean, origin: string, logMessage: string, timestamp: string){
+        this.status = status;
+        this.origin = origin;
+        this.logMessage = logMessage;
+        this.timestamp = timestamp;
+    }
+}
+
+const readLogs = (inputFileName: string) => {
+    const logDir = path.join(__dirname, '..', '..', 'logs');
+    const logFile = path.join(logDir, inputFileName);
+
+    fs.readFile(logFile, 'utf-8', (err, data)=>{
+        if(err){
+            console.log("Logs For The File Does Not Exist");
+            process.exit(1);
+        }
+
+        JSON.parse(data.trim()).map((d: LogData)=>{
+            const logData = new LogDataRenderer(d.status, d.origin, d.logMessage, d.timestamp);
+            console.table(logData);
+        })
+    })
+}
+
+rl.question('Filename: ', name => {
+    readLogs(name);
+    rl.close;
+});
+
+export default readLogs;
